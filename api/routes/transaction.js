4f6e142ba82d0174262bfc9c343d692ce3c5115b
Eric Valera Miller <eric@elericuz.com>
2022-06-28 11:03:12 -0500

    endpoint showing transactions

--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const TransactionController = require('../controllers/transaction')
 
-router.get('/get/*?', TransactionController.get)
+router.get('/get', TransactionController.get)
 router.post('/add', TransactionController.add);
 
 module.exports = router;
