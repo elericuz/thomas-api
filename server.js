724883cd3b35eca71f07129f6b5f8c0e38b65f0d
Eric Valera Miller <eric@elericuz.com>
2022-12-18 18:03:18 -0500

    moving env variables

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+require('dotenv').config();
 
 // Constants
 const http = require('http');
@@ -16,11 +17,13 @@ mongoose.set('useCreateIndex', true);
 
 let dbUri = "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_SERVER + "/" + process.env.MONGO_PORT + "?authSource=admin&retryWrites=true&w=1"
 if (process.env.ENVIRONMENT == "DEVELOPMENT") {
-   dbUri = "mongodb://" + process.env.MONGO_USER_DEV + ":" + process.env.MONGO_PASSWORD_DEV + "@" + process.env.MONGO_SERVER_DEV + "/" + process.env.MONGO_DB_DEV + "?authSource=admin&retryWrites=true&w=1"
+    dbUri = "mongodb://" + process.env.MONGO_USER_DEV + ":" + process.env.MONGO_PASSWORD_DEV + "@" + process.env.MONGO_SERVER_DEV + "/" + process.env.MONGO_DB_DEV + "?authSource=admin&retryWrites=true&w=1"
 }
 
 mongoose.connect(dbUri, {useNewUrlParser: true, useUnifiedTopology: true})
-   .then((result) => { server.listen(PORT, HOST) })
-   .catch((err) => console.log(err));
+    .then((result) => {
+        server.listen(PORT, HOST)
+    })
+    .catch((err) => console.log(err));
 
 console.log(`Running on https://${HOST}:${PORT}`);
