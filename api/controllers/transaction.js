572be72f29c2d4162ca6c818b1ac354dd1ae52d3
Eric Valera Miller <eric@elericuz.com>
2022-12-19 22:58:27 -0500

    adjusting timezone

--- api/controllers/transaction.js
+++ api/controllers/transaction.js
@@ -105,11 +105,11 @@ exports.add = async (req, res, next) => {
 }
 
 exports.brief = async (req, res, next) => {
-    let startDate = moment().tz('America/Lima')
+    let startDate = moment().add(-5, 'hours').tz('America/Lima')
         .subtract(5, 'minute')
         .startOf('minute')
         .format()
-    let endDate = moment().tz('America/Lima')
+    let endDate = moment().add(-5, 'hours').tz('America/Lima')
         .endOf('minute')
         .format()
 
